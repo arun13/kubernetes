b2c7b227c9e1df51cbbbf938b9ac1fc3b13e3714
arun13 <ar.taneja@gmail.com>
2018-11-29 19:35:53 -0500

    Jenkins file update

--- account-deposit/k8s/deploy.sh
+++ account-deposit/k8s/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
- kubectl apply -f ./account/k8s/account-deposit-deployment.tpl.yml
- kubectl apply -f ./account/k8s/account-deposit-service.tpl.yml
+ kubectl apply -f ./account-deposit/k8s/account-deposit-deployment.tpl.yml
+ kubectl apply -f ./account-deposit/k8s/account-deposit-service.tpl.yml
--- account-withdrawal/k8s/deploy.sh
+++ account-withdrawal/k8s/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
- kubectl apply -f ./account/k8s/account-withdrawal-deployment.tpl.yml
- kubectl apply -f ./account/k8s/account-withdrawal-service.tpl.yml
+ kubectl apply -f ./account-withdrawal/k8s/account-withdrawal-deployment.tpl.yml
+ kubectl apply -f ./account-withdrawal/k8s/account-withdrawal-service.tpl.yml
